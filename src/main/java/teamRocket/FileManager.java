c23538230dcc0542afd7056734a464a52b14e8e1
Douglas Lee <dlee73@hawkmail.hccfl.edu>
2021-03-25 20:28:25 -0400

    Added stub methods for files persistence and table.

--- src/main/java/teamRocket/FileManager.java
+++ src/main/java/teamRocket/FileManager.java
@@ -1,17 +1,25 @@
 package teamRocket;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class FileManager {
+public class FileManager implements ActionListener{
 	private JFrame frame;
 	private JPanel contentpanel;
 	private JPanel labelpanel;
 	private JPanel buttonpanel;
+	private JTable table;
 	private JLabel label;
 	private JButton addFile;
 	private JButton removeFile;
+	
+	private enum Actions{
+		ADD,
+		REMOVE
+	}
 	
 	public FileManager() {
 		frame = new JFrame("File Manager");
@@ -21,8 +29,17 @@ public class FileManager {
 		labelpanel = new JPanel();
 		buttonpanel = new JPanel();
 		
+		table = new JTable(20, 2);
+		table.setValueAt("File Path", 0, 0);
+		table.setValueAt("Last Editted", 0, 1);
+		
 		addFile = new JButton("Add Files");
+		addFile.setActionCommand(Actions.ADD.name());
+		addFile.addActionListener(this);
+		
 		removeFile = new JButton("Remove Files");
+		removeFile.setActionCommand(Actions.REMOVE.name());
+		removeFile.addActionListener(this);
 		
 		label = new JLabel("Here there be files");
 		label.setFont(new Font("Times New Roman", Font.BOLD + Font.ITALIC, 24));
@@ -36,9 +53,25 @@ public class FileManager {
 		buttonpanel.add(addFile);
 		buttonpanel.add(removeFile);
 		
+		
+		
 		contentpanel.add(labelpanel, BorderLayout.PAGE_START);
 		contentpanel.add(buttonpanel, BorderLayout.PAGE_END);
+		contentpanel.add(table, BorderLayout.CENTER);
 		frame.setVisible(true);
 	}
+	
+	// Unimplemented buttons to add and remove file persistence.
+	
+	/*
+	public void actionPerformed(ActionEvent e) {
+		if (evt.getActionCommand() == Actions.ADD.name()) {
+			// Add file method
+		}
+		if (evt.getActionCommand() == Actions.REMOVE.name()) {
+			// remove file method
+		}
+	}
+	*/
 	
 }
